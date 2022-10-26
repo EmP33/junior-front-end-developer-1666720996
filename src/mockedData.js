c4f70707647666d3d56322344a995e973f44279f
EmP33 <purwinmarek16@gmail.com>
2022-10-26 09:18:54 +0200

    Add mocked tasks data

---
+++ src/mockedData.js
@@ -0,0 +1,92 @@
+export const mockedTasks = [
+  {
+    title: "Application Setup",
+    status: "completed",
+    context: [
+      {
+        title: "Business Context Title 1",
+        description: "Business Context Description 1",
+      },
+      {
+        title: "Business Context Title 2",
+        description: "Business Context Description 2",
+      },
+      {
+        title: "Business Context Title 3",
+        description: "Business Context Description 3",
+      },
+    ],
+  },
+  {
+    title: "Static Books List",
+    status: "completed",
+    context: [
+      {
+        title: "Business Context Title 1",
+        description: "Business Context Description 1",
+      },
+      {
+        title: "Business Context Title 2",
+        description: "Business Context Description 2",
+      },
+      {
+        title: "Business Context Title 3",
+        description: "Business Context Description 3",
+      },
+    ],
+  },
+  {
+    title: "Administration Panel",
+    status: "completed",
+    context: [
+      {
+        title: "Business Context Title 1",
+        description: "Business Context Description 1",
+      },
+      {
+        title: "Business Context Title 2",
+        description: "Business Context Description 2",
+      },
+      {
+        title: "Business Context Title 3",
+        description: "Business Context Description 3",
+      },
+    ],
+  },
+  {
+    title: "Connect Admin with Frontend",
+    status: "active",
+    context: [
+      {
+        title: "Business Context Title 1",
+        description: "Business Context Description 1",
+      },
+      {
+        title: "Business Context Title 2",
+        description: "Business Context Description 2",
+      },
+      {
+        title: "Business Context Title 3",
+        description: "Business Context Description 3",
+      },
+    ],
+  },
+  {
+    title: "Book Review Feature",
+    status: "blocked",
+    context: [
+      {
+        title: "Business Context Title 1",
+        description: "Business Context Description 1",
+      },
+      {
+        title: "Business Context Title 2",
+        description: "Business Context Description 2",
+      },
+      {
+        title: "Business Context Title 3",
+        description: "Business Context Description 3",
+      },
+    ],
+  },
+];
