046b9b8a7b5841d9f8d6497de95feee0e5040539
EmP33 <purwinmarek16@gmail.com>
2022-10-27 14:12:07 +0200

    Moved container from App.jsx to HomePage.jsx, create  routes

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+// Components
+import ContextCard from "../components/ContextCard/ContextCard";
+// Constants
+import { mockedTasks } from "../mockedData";
+// Icons
+import { IoIosCheckmark } from "react-icons/io";
+import { BiRightArrowAlt } from "react-icons/bi";
+import { TiLockClosedOutline } from "react-icons/ti";
+import { AiOutlineCompass } from "react-icons/ai";
+// Assets
+import avatar from "../assets/frame.png";
+// Utils
+import {
+  getMonthName,
+  addZeroToDate,
+  formatDisplayData,
+} from "../utils/date-formaters";
+
+const HomePage = () => {
+  const displayedDate = formatDisplayData(
+    mockedTasks[0].contexts[0].created_at
+  );
+  return (
+    <div className="container">
+      <section className="tasks-section">
+        <h3 className="tasks-section__heading">YOUR TASKS</h3>
+        <ul className="tasks">
+          {mockedTasks.map((task) => (
+            <li key={task.id} className={`tasks__task ${task.status}`}>
+              {task.status === "completed" ? (
+                <IoIosCheckmark />
+              ) : task.status === "active" ? (
+                <BiRightArrowAlt />
+              ) : (
+                <TiLockClosedOutline />
+              )}
+              <span>{task.title}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className="context-container">
+        <div className="context-container__heading">
+          <h3>
+            <AiOutlineCompass /> BUSINESS CONTEXT
+          </h3>
+        </div>
+        <div className="business-contexts">
+          {mockedTasks[0].contexts.map((context) => (
+            <ContextCard key={context.id} context={context} />
+          ))}
+        </div>
+        <div className="context-overview">
+          <h2>{mockedTasks[0].contexts[0].title}</h2>
+          <div className="context-overview__content">
+            <img src={avatar} alt="avatar" />
+            <div className="content-heading">
+              <span>{mockedTasks[0].contexts[0].author}</span>
+              <span>
+                Today, {addZeroToDate(displayedDate.getDate())}th{" "}
+                {getMonthName(displayedDate.getTime())}
+              </span>
+              <span>
+                {addZeroToDate(displayedDate.getHours())}:
+                {addZeroToDate(displayedDate.getMinutes())}
+              </span>
+            </div>
+            <p>{mockedTasks[0].contexts[0].content}</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
